c22bba46c31411b327642225b8846d7a5e399d9f
smrt <alexx.smrt@gmail.com>
2014-01-17 20:18:59 +0400

    Config db update

--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,13 @@
 
 // ** Параметры MySQL: Эту информацию можно получить у вашего хостинг-провайдера ** //
 /** Имя базы данных для WordPress */
-define('DB_NAME', 'germes');
+define('DB_NAME', 'travel2u_germes');
 
 /** Имя пользователя MySQL */
-define('DB_USER', 'germes');
+define('DB_USER', 'travel2u_germes');
 
 /** Пароль к базе данных MySQL */
-define('DB_PASSWORD', '123');
+define('DB_PASSWORD', 'G10N2Q8i');
 
 /** Имя сервера MySQL */
 define('DB_HOST', 'localhost');
